f341f940bf1308035bd2ead47599e7f58fa28365
kirya <misakikirya345@gmail.com>
2021-11-28 12:11:24 +0300

    second commit

--- ussd/db.php
+++ ussd/db.php
@@ -1,25 +1,5 @@
 <?php
 
-$sqlicon = new mysqli("localhost", "root", "");
+$sqlicon = new mysqli("localhost", "root", "4Aj[*n[o4QQ7Nvj4","muk_link_up"); 
 
-$sqlicon->query("CREATE DATABASE IF NOT EXISTS muk_link_up");
-
-mysqli_select_db($sqlicon, "muk_link_up");
-
-$string = $sqlicon->query("CREATE TABLE IF NOT EXISTS users(id int(10) not null auto_increment, primary key(id), phone_number varchar(11) not null unique, user_name varchar(50) not null, residence varchar(30) not null, gender enum('M', 'F') NOT NULL, age int(20) not null)");
-$string = $sqlicon->query("INSERT INTO users(id, phone_number,user_name, residence, gender, age)VALUES(1999,'07888866+88','kirya', 'kayunga', 'male', '45')");
-
-if ($string){
-	echo "connected";
-} else{
-	echo $sqlicon->error;
-}
-
-
-
-
-
-
-
-
-
+$sqlicon->query("CREATE TABLE IF NOT EXISTS users(id int(10) not null auto_increment, primary key(id), phone_number varchar(11) not null unique, user_name varchar(50) not null, residence varchar(30) not null, gender enum('M', 'F') NOT NULL, age int(20) not null)");
